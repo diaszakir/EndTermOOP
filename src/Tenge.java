32b0920030c5d9a8c9cc1a7b24ae8c5092ffcd7e
diaszakir <153705027+diaszakir@users.noreply.github.com>
2024-02-21 13:41:05 +0600

    Tenge.java

--- src/Tenge.java
+++ src/Tenge.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class Tenge {
+public class Tenge extends Money{
+    public Tenge(double money) {
+        super(money);
+    }
+
+    @Override
+    void exchangeToDollar() {
+        double dollar = money / 450;
+        System.out.println(dollar);
+    }
+
+    @Override
+    void exchangeToTenge() {
+    }
+
+    @Override
+    void exchangeToRubles() {
+        double rubles = money / 5.0;
+        System.out.println(rubles);
+    }
+
+    @Override
+    void exchangeToEuro() {
+        double euros = money / 480.0;
+        System.out.println(euros);
+    }
 }
