7458e30097c7ce78d8b26234acf694a7cd815c82
diaszakir <153705027+diaszakir@users.noreply.github.com>
2024-02-21 13:42:34 +0600

    Dollars.java

--- src/Dollars.java
+++ src/Dollars.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class Dollars {
+public class Dollars extends Money{
+
+    public Dollars(double money) {
+        super(money);
+    }
+
+    @Override
+    void exchangeToDollar() {
+    }
+
+    @Override
+    void exchangeToTenge() {
+        double tenge = money * 450;
+        System.out.println(tenge);
+    }
+
+    @Override
+    void exchangeToRubles() {
+        double rubles = money * 92;
+        System.out.println(rubles);
+    }
+
+    @Override
+    void exchangeToEuro() {
+        double euros = money * 0.93;
+        System.out.println(euros);
+    }
 }
