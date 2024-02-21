fbff70532d7e6ef1b95424a1c1b34a43242cb1a8
diaszakir <153705027+diaszakir@users.noreply.github.com>
2024-02-21 13:40:31 +0600

    Rubles.java

--- src/Rubles.java
+++ src/Rubles.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class Rubles {
+public class Rubles extends Money{
+    public Rubles(double money) {
+        super(money);
+    }
+
+    @Override
+    void exchangeToDollar() {
+        double dollar = money / 92;
+        System.out.println(dollar);
+    }
+
+    @Override
+    void exchangeToTenge() {
+        double tenge = money * 5;
+        System.out.println(tenge);
+    }
+
+    @Override
+    void exchangeToRubles() {
+    }
+
+    @Override
+    void exchangeToEuro() {
+        double euros = money / 99;
+        System.out.println(euros);
+    }
 }
